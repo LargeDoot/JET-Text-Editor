4a717e2b5e6ac7d94ce71345e867983608a9c8c9
Ethan <ethanwilson.2000@gmail.com>
2020-03-17 16:26:13 +0000

    Fix bug with saving files where text would get written to the JETFile object even if the save operation is cancelled by the user. This also causes the difference check on window close to malfunction and doesnt prompt user to save correctly.

--- src/com/company/listeners/fileMenuListeners/fileSaveAsListener.java
+++ src/com/company/listeners/fileMenuListeners/fileSaveAsListener.java
@@ -2,6 +2,7 @@ package com.company.listeners.fileMenuListeners;
 
 import com.company.EditorWindow;
 import com.company.FileBrowser;
+import com.company.JETFile;
 import com.company.ReaderWriter;
 
 import java.awt.event.ActionListener;
@@ -13,9 +14,30 @@ public class fileSaveAsListener extends fileSaveListener implements ActionListen
     }
 
     @Override
-    void saveFile() {
-        fileToSave.setFileLocation(new FileBrowser().getURI());
-        new ReaderWriter(fileToSave);
+    void saveFile(JETFile file) {
+
+        String saveAddress = "";
+        String originalAddress = file.getFileLocation();
+
+        //Open a new file browser to get a save URI
+        saveAddress = new FileBrowser().getURI();
+
+        //Set the file's location to the chosen URI as long as the user didn't cancel.
+        if (saveAddress != null) {
+            file.setFileLocation(saveAddress);
+        }
+
+        //If the file now has a new save address, write the data (i.e if user didnt cancel file browser).
+        if (originalAddress.equals(saveAddress)) {
+
+            //Set the text contents of the file to that inside the main textarea.
+            file.setTextContents(window.getText());
+
+            //Write the data
+            new ReaderWriter(file);
+
+        }
+
     }
 
 }
--- src/com/company/listeners/fileMenuListeners/fileSaveListener.java
+++ src/com/company/listeners/fileMenuListeners/fileSaveListener.java
@@ -13,38 +13,80 @@ public class fileSaveListener implements ActionListener {
 
     final EditorWindow window;
     JDialog dialog;
-    JETFile fileToSave;
 
+    /**
+     * Constructor to create a listener from a window
+     *
+     * @param window the parent window
+     */
     public fileSaveListener(EditorWindow window) {
 
         this.window = window;
     }
 
+    /**
+     * A constructor to create a listener from a dialog (i.e save dialog)
+     *
+     * @param window the parent window of the dialog
+     * @param dialog the parent dialog
+     */
     public fileSaveListener(EditorWindow window, JDialog dialog) {
 
         this.dialog = dialog;
         this.window = window;
+
     }
 
+    /**
+     * Get the current file and store it, then set the contents of the file ONLY if the file has an address. This
+     * ensures reliability when checking for changes on window close. The file can then be closed as long as an address
+     * is provided or retrieved from a file browser.
+     *
+     * @param actionEvent the event
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
-        this.fileToSave = window.getCurrentFile();
-        fileToSave.setTextContents(window.getText());
-
-        saveFile();
+        saveFile(window.getCurrentFile());
 
     }
 
-    void saveFile() {
-        if (fileToSave.getFileLocation().equals("Untitled")) {
+    /**
+     * A method to save files, where if no file location is provided, it will open a new file browser where the user can
+     * choose a new location. If the user cancels the file browser operation, the file will not be saved.
+     */
+    void saveFile(JETFile file) {
 
-            fileToSave.setFileLocation(new FileBrowser().getURI());
+        String saveAddress = file.getFileLocation();
+
+        //Check that the save location is not the default "Untitled" which is set when creating an empty JETFile
+        // without an address.
+        if (file.getFileLocation().equals("Untitled")) {
+
+            //Open a new file browser to get a save URI
+            saveAddress = new FileBrowser().getURI();
+
+            //Set the file's location to the chosen URI as long as the user didn't cancel.
+            if (saveAddress != null) {
+                file.setFileLocation(saveAddress);
+            }
+
         }
 
-        new ReaderWriter(fileToSave);
+        //If the file now has a save address, write the data (i.e if user didnt cancel file browser).
+        if (!"Untitled".equals(saveAddress)) {
 
-        if (dialog != null) {
+            //Set the text contents of the file to that inside the main textarea.
+            file.setTextContents(window.getText());
+
+            //Write the data
+            new ReaderWriter(file);
+
+        }
+
+
+        //If a dialog was parsed in the constructor, then dispose of it now.
+        if (dialog != null && saveAddress != null) {
 
             window.dispose();
         }
